909820b04f902187ba3212fb0f6840f3476433d3
Dhruv Sharma <dhruv.sharma@magicsw.com>
2021-05-25 12:14:08 +0530

    PLAYER INPUT RECEIVED AND COMPUTER SYMBOL CALCULATED

--- src/main/java/com/workshop/ticTacToe.java
+++ src/main/java/com/workshop/ticTacToe.java
@@ -4,15 +4,33 @@ import java.util.*;
 public class ticTacToe {
     public static void main(String[] args){
         char[] board=createBoard();
-        for(int i=1;i<10;i++) {
-            board[i]='-';
-            System.out.print(board[i] + " " + "|");
+        for(int i=0;i<10;i++) {
+            board[i] = '-';
+            System.out.print(board[i] + "|");
         }
+        System.out.println();
+        char playerSymbol=gameInput();
+        char computerSymbol;
+        if(playerSymbol=='X' || playerSymbol=='O'){
+            System.out.println("player input received");
+        }
+        else{
+            System.out.println("please input a valid response (X or O)");
+        }
+        computerSymbol=(playerSymbol=='X')? 'O' :'X';
+        System.out.println(computerSymbol);
+
     }
 
     public static char[] createBoard(){
         char[] board=new char[10];
         return board;
+    }
+
+    public static char gameInput(){
+        Scanner scn=new Scanner(System.in);
+        char inputXO=scn.next().charAt(0);
+        return inputXO;
     }
 
 }
