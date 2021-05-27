4bf5d7914c4b7c0a80981b04fd680eae756a098b
Dhruv Sharma <dhruv.sharma@magicsw.com>
2021-05-27 09:52:13 +0530

    ADDED LOGIC FOR COMPLETION OF GAME

--- src/main/java/com/workshop/ticTacToe.java
+++ src/main/java/com/workshop/ticTacToe.java
@@ -1,94 +1,122 @@
 package com.workshop;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ticTacToe {
-    public static void main(String[] args){
-        int heads=0;
-        int tails=1;
-        char[] board=createBoard();
-        displayBoard(board);
-        char playerSymbol=gameInput();
-        char computerSymbol;
+    Scanner scanner = new Scanner(System.in);
 
-        if(playerSymbol=='X' || playerSymbol=='O'){
-            System.out.println("player input received");
-        }
-        else{
-            System.out.println("please input a valid response (X or O)");
-
-        }
-        computerSymbol=(playerSymbol=='X')? 'O' :'X';
-        System.out.println(computerSymbol);
-        Scanner scanner=new Scanner((System.in));
-        int location=scanner.nextInt();
-        while(!checkInputLocation(location)){
-            System.out.println("This board position is not available.. Please input again");
-            location=scanner.nextInt();
-        }
-        System.out.println("This position is available");
-        board[location]=playerSymbol;
-        int outcome=toss();
-        if(outcome==heads){
-            System.out.println("Heads, User may start the game");
-        }
-        else{
-            System.out.println("Tails, Computer will start the game");
-        }
-
-
-
+    public static void main(String[] args) {
+        System.out.println("welcome to TicTacToe");
+        Scanner myScan = new Scanner(System.in);
+        char ans;
+        do {
+            ticTacToe ticTacToe = new ticTacToe();
+            char[] board = ticTacToe.createBoard();
+            char computer;
+            char player = ticTacToe.chooseLetter();
+            computer = (player == 'X') ? '0' : 'X';
+            System.out.println("Player : " + player);
+            System.out.println("Computer : " + computer);
+            ticTacToe.showBoard(board);
+            int options = (int) (Math.random() * 2);
+            if (options == 0) {
+                System.out.println("You will play first");
+            } else {
+                System.out.println("Computer will play first");
+            }
+            boolean play = true;
+            while (play) {
+                if (options == 0) {
+                    board = ticTacToe.userInput(board, player);
+                    ticTacToe.showBoard(board);
+                    System.out.println("******************************");
+                    play = ticTacToe.winCheck(board, player, computer);
+                    if (play) {
+                        board = ticTacToe.cpuInput(board, player, computer);
+                        ticTacToe.showBoard(board);
+                        System.out.println("******************************");
+                        play = ticTacToe.winCheck(board, player, computer);
+                        if (!play)
+                            System.out.println("Game Over");
+                    } else
+                        System.out.println("Game Over");
+                } else {
+                    board = ticTacToe.cpuInput(board, player, computer);
+                    ticTacToe.showBoard(board);
+                    System.out.println("******************************");
+                    play = ticTacToe.winCheck(board, player, computer);
+                    if (play) {
+                        board = ticTacToe.userInput(board, player);
+                        ticTacToe.showBoard(board);
+                        System.out.println("******************************");
+                        play = ticTacToe.winCheck(board, player, computer);
+                        if (!play)
+                            System.out.println("Game Over");
+                    } else
+                        System.out.println("Game Over");
+                }
+            }
+            System.out.println("Do you want to play again(y/n) ?");
+            ans = myScan.next().charAt(0);
+        } while (ans == 'Y' || ans == 'y');
 
     }
 
-    public static char[] createBoard(){
-        char[] board={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+    public char[] createBoard() {
+        char[] board = new char[9];
+        Arrays.fill(board, ' ');
         return board;
     }
 
-    public static char gameInput(){
-        Scanner scn=new Scanner(System.in);
-        char inputXO=scn.next().charAt(0);
-        return inputXO;
-    }
-    public static void displayBoard(char[] board){
-        for(int j=1;j<=3;j++){
-            System.out.print(board[j]+ " " + "|");
-        }
-        System.out.println();
-        for(int j=4;j<=6;j++){
-            System.out.print(board[j]+ " " + "|");
-        }
-        System.out.println();
-        for(int j=7;j<=9;j++){
-            System.out.print(board[j]+ " " + "|");
-        }
-
-    }
-    public static boolean checkInputLocation(int location){
-        char[] board=new char[10];
-        return board[location]==' ';
+    public char chooseLetter() {
+        System.out.println("Choose 'X' or '0'");
+        return scanner.next().charAt(0);
 
     }
 
-    public static int toss(){
-        int outcome=(int) (Math.random() *2);
-        return outcome;
+
+    public void showBoard(char[] board) {
+        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("----------");
+        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("----------");
+        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
     }
 
-    public int gamePlay(char[] board, char playerSymbol, char computerSymbol){
-        if ((board[0] == playerSymbol && board[1] == playerSymbol && board[2] == playerSymbol) || (board[3] == playerSymbol && board[4] == playerSymbol && board[5] == playerSymbol)
-                || (board[6] == playerSymbol && board[7] == playerSymbol && board[8] == playerSymbol) || (board[0] == playerSymbol && board[3] == playerSymbol && board[6] == playerSymbol)
-                || (board[1] == playerSymbol && board[4] == playerSymbol && board[7] == playerSymbol) || (board[2] == playerSymbol && board[5] == playerSymbol && board[8] == playerSymbol)
-                || (board[0] == playerSymbol && board[4] == playerSymbol && board[8] == playerSymbol) || (board[2] == playerSymbol && board[4] == playerSymbol && board[6] == playerSymbol)) {
-            System.out.println("User has won the match");
-            return 0;
+    public char[] userInput(char[] board, char user) {
+        int input = 0;
+        do {
+            System.out.println("Choose the number 1-9");
+            int location = scanner.nextInt();
+            for (int i = 0; i < board.length; i++) {
+                if (i == location - 1 && board[i] == ' ') {
+                    board[i] = user;
+                    input = 1;
+                    break;
+                }
+            }
+            if (input == 0)
+                System.out.println("Please Enter correct input");
+        } while (input == 0);
+        return board;
+
+    }
+
+    public boolean winCheck(char[] board, char player, char computer) {
+        if ((board[0] == player && board[1] == player && board[2] == player) || (board[3] == player && board[4] == player && board[5] == player)
+                || (board[6] == player && board[7] == player && board[8] == player) || (board[0] == player && board[3] == player && board[6] == player)
+                || (board[1] == player && board[4] == player && board[7] == player) || (board[2] == player && board[5] == player && board[8] == player)
+                || (board[0] == player && board[4] == player && board[8] == player) || (board[2] == player && board[4] == player && board[6] == player)) {
+            System.out.println("You won");
+            return false;
         }
-        if ((board[0] == computerSymbol && board[1] == computerSymbol && board[2] == computerSymbol) || (board[3] == computerSymbol && board[4] == computerSymbol && board[5] == computerSymbol)
-                || (board[6] == computerSymbol && board[7] == computerSymbol && board[8] == computerSymbol) || (board[0] == computerSymbol && board[3] == computerSymbol && board[6] == computerSymbol)
-                || (board[1] == computerSymbol && board[4] == computerSymbol && board[7] == computerSymbol) || (board[2] == computerSymbol && board[5] == computerSymbol && board[8] == computerSymbol)
-                || (board[0] == computerSymbol && board[4] == computerSymbol && board[8] == computerSymbol) || (board[2] == computerSymbol && board[4] == computerSymbol && board[6] == computerSymbol)) {
-            System.out.println("Computer has won the match");
-            return 0;
+        if ((board[0] == computer && board[1] == computer && board[2] == computer) || (board[3] == computer && board[4] == computer && board[5] == computer)
+                || (board[6] == computer && board[7] == computer && board[8] == computer) || (board[0] == computer && board[3] == computer && board[6] == computer)
+                || (board[1] == computer && board[4] == computer && board[7] == computer) || (board[2] == computer && board[5] == computer && board[8] == computer)
+                || (board[0] == computer && board[4] == computer && board[8] == computer) || (board[2] == computer && board[4] == computer && board[6] == computer)) {
+            System.out.println("Computer won");
+            return false;
         }
         int flag = 0;
         for (int i = 0; i < 9; i++) {
@@ -99,11 +127,11 @@ public class ticTacToe {
         }
         if (flag == 0) {
             System.out.println("Match Tied");
-            return 0;
+            return false;
         }
-        return 1;
-
+        return true;
     }
+
     public boolean winning(char[] tempBoard, char mark) {
         return (tempBoard[0] == mark && tempBoard[1] == mark && tempBoard[2] == mark) || (tempBoard[3] == mark && tempBoard[4] == mark && tempBoard[5] == mark)
                 || (tempBoard[6] == mark && tempBoard[7] == mark && tempBoard[8] == mark) || (tempBoard[0] == mark && tempBoard[3] == mark && tempBoard[6] == mark)
@@ -171,6 +199,5 @@ public class ticTacToe {
         return board;
     }
 
+
 }
-
-
